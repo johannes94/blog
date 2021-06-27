9c5103d205a7acf7536bb96cc406b854e4268083
Johannes <johannes.malsam.b@gmail.com>
2021-06-27 18:03:00 +0200

    added age to userdata for crud-rest-api

--- posts/crud-rest-api-with-gorilla-mux/03_sorting_paging_filter/main.go
+++ posts/crud-rest-api-with-gorilla-mux/03_sorting_paging_filter/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"reflect"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"gorm.io/driver/postgres"
@@ -18,12 +24,62 @@ type User struct {
 	Email     string `json:"email"`
 }
 
+func dropTable(db *gorm.DB) error {
+	return db.Exec("DROP TABLE users;").Error
+}
+
+func initUsers(db *gorm.DB) error {
+	data, err := ioutil.ReadFile("userdata.json")
+	if err != nil {
+		return err
+	}
+
+	var users []User
+	if err := json.Unmarshal(data, &users); err != nil {
+		return err
+	}
+
+	return db.Create(&users).Error
+}
+
+func handleFlags(db *gorm.DB) {
+	initData := flag.Bool("init-data", false, "Set this flag if DB should be initialized with dummy data")
+	drop := flag.Bool("drop-table", false, "Set this flag if you wan't to drop all user data in your DB")
+	flag.Parse()
+
+	if *drop {
+		msg := ""
+		if err := dropTable(db); err != nil {
+			msg = fmt.Sprintf("Error dropping table: %v", err)
+		} else {
+			msg = "Dropped users table in DB"
+		}
+
+		fmt.Println(msg)
+		os.Exit(0)
+	}
+
+	if *initData {
+		msg := ""
+		if err := initUsers(db); err != nil {
+			msg = fmt.Sprintf("Error initializing data in DB: %v", err)
+		} else {
+			msg = "Initialized data in DB."
+		}
+
+		fmt.Println(msg)
+		os.Exit(0)
+	}
+}
+
 func main() {
 
 	db, err := initDB()
 	if err != nil {
 		fmt.Printf("Error initializing DB: %v\n", err)
 	}
+
+	handleFlags(db)
 
 	uh := userHandler{db: db}
 
@@ -51,9 +107,66 @@ type userHandler struct {
 	db *gorm.DB
 }
 
+var userFields = getUserFields()
+
+func getUserFields() []string {
+	var field []string
+
+	v := reflect.ValueOf(User{})
+	for i := 0; i < v.Type().NumField(); i++ {
+		field = append(field, v.Type().Field(i).Tag.Get("json"))
+	}
+
+	return field
+}
+
+func stringInSlice(strSlice []string, s string) bool {
+	for _, v := range strSlice {
+		if v == s {
+			return true
+		}
+	}
+
+	return false
+}
+
+func sortByToGormQuery(sortBy string) (string, error) {
+	splits := strings.Split(sortBy, ".")
+	if len(splits) != 2 {
+		return "", errors.New("malformed sortBy query parameter, should be field.orderdirection")
+	}
+
+	field := splits[0]
+	order := splits[1]
+
+	if order != "desc" && order != "asc" {
+		return "", errors.New("malformed orderdirection in sortBy query paramater, should be asc or desc")
+	}
+
+	if !stringInSlice(userFields, field) {
+		return "", errors.New("unknown field in sortBy query paramater")
+	}
+
+	return fmt.Sprintf("%s %s", field, strings.ToUpper(order)), nil
+
+}
+
 func (uh userHandler) getAllUsers(w http.ResponseWriter, r *http.Request) {
 	users := []User{}
-	if err := uh.db.Find(&users).Error; err != nil {
+
+	// sort by is expected to be in form of field.orderdirection i. e. id.asc, with id.asc as default value
+	sortBy := r.URL.Query().Get("orderBy")
+	if sortBy == "" {
+		sortBy = "id.asc"
+	}
+
+	gormSort, err := sortByToGormQuery(sortBy)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := uh.db.Order(gormSort).Find(&users).Error; err != nil {
 		fmt.Println(err)
 		http.Error(w, "Error on DB find for all users", http.StatusInternalServerError)
 		return
